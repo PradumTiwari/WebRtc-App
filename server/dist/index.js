0b8e095c3d5e781cc0a9e47293e76b410fb778f0
Pradum <satendarpradum@gmail.com>
2024-09-22 19:42:49 +0530

    Basic Setup for Socket io

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,2 +1,25 @@
 "use strict";
-console.log("Hello world");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const http_1 = __importDefault(require("http"));
+const serverConfig_1 = require("./config/serverConfig");
+const socket_io_1 = require("socket-io");
+const cors_1 = __importDefault(require("cors"));
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+const server = http_1.default.createServer(app);
+console.log("PORT ", serverConfig_1.PORT);
+const io = new socket_io_1.Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+    }
+});
+io.on("connection", (socket) => {
+});
+server.listen(serverConfig_1.PORT, () => {
+    console.log(`Server is up at port ${serverConfig_1.PORT}`);
+});
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,33 @@
-console.log("Hello world");
+import express from "express";
+import http from "http";
+import {PORT} from "./config/serverConfig";
+import {Server} from "socket.io";
+import cors from "cors";
+const app=express();
+
+app.use(cors());
+
+const server=http.createServer(app);
+console.log("PORT ",PORT);
+
+
+const io=new Server(server,{
+    cors:{
+        origin:"*",
+        methods:["GET","POST"],
+    }
+});
+
+
+io.on("connection",(socket)=>{
+    console.log("New User connection");
+
+    socket.on("disconnect",()=>{
+        console.log("User Disconnected");
+        
+    })
+})
+server.listen(PORT,()=>{
+    console.log(`Server is up at port ${PORT}`);
+
+})
